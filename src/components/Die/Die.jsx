16cf4aa17e8d48057206adee93b7563aace03ffe
Massimo Tascone <massimo@Massi.local>
2023-04-22 16:46:03 +0200

    passed function to dive component logic id + nanoid package

--- src/components/Die/Die.jsx
+++ src/components/Die/Die.jsx
@@ -1,8 +1,11 @@
 import "./Die.css";
 
 export function Die(props) {
+  const dieStyle = {
+    backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF",
+  };
   return (
-    <div className="die">
+    <div className="die" onClick={props.handleClick} style={dieStyle}>
       <h2>{props.value}</h2>
     </div>
   );
--- src/components/PushButton/PushButton.jsx
+++ src/components/PushButton/PushButton.jsx
@@ -2,7 +2,7 @@ import "./PushButton.css";
 
 export function PushButton(props) {
   return (
-    <button className="btn" onClick={props.handleClick}>
+    <button className="btn" onClick={props.handleRoll}>
       <span className="btn--shadow"></span>
       <span className="btn--edge"></span>
       <span className="btn--front">{props.name}</span>
