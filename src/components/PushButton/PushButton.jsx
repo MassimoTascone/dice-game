1461b796314e1449b0aa1c13292017dd89fb7781
Massimo Tascone <massimo@Massi.local>
2023-04-24 14:32:33 +0200

    added translation + rollcounter + keep nbr logic

--- src/components/PushButton/PushButton.jsx
+++ src/components/PushButton/PushButton.jsx
@@ -5,7 +5,9 @@ export function PushButton(props) {
     <button className="btn" onClick={props.handleRoll}>
       <span className="btn--shadow"></span>
       <span className="btn--edge"></span>
-      <span className="btn--front">{props.name}</span>
+      <span className="btn--front">
+        {props.lang === "eng" ? props.content.eng.btn : props.content.fr.btn}
+      </span>
     </button>
   );
 }
